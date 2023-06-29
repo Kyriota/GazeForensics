f361054765990d2d5f9ffc00fb679f24df8eb4d2
Kyriota <943422656@qq.com>
2023-06-30 03:19:25 +0800

    Finished evaluation, reproducibility, etc.

--- Config.py
+++ Config.py
@@ -1,62 +1,75 @@
 import torchvision.transforms as transforms
 
 
-
-train_DS_name = 'CDF'
-test_DS_name = 'CDF'
 rootDir = '/home/kyr/GazeForensicsData/'
 
-standard_transform = transforms.Compose([
-    transforms.Resize((224,224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+
+class Config:
+    def __init__(self):
+
+        train_DS_name = 'WDF'
+        test_DS_name = 'WDF'
+
+        standard_transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
 
 
+        params_basic = { # Fundamental parameters
+            'rootDir': rootDir,
+            'train_DS_name': train_DS_name,
+            'test_DS_name': test_DS_name,
+            'checkpointDir': 'Checkpoints/',
+            'tryID': None, # 'tryID' is datetime in yymmdd_HHMMSS string
+        }
+        params_model = { # Parameters that controls model
+            'checkpoint': None, # None stands for init a new model, otherwise load the model from the path
+            'seed': 64, # This seed if for initializing the model
+            'leaky': 0, # How many dimensions to expand on gaze embedding
+        }
+        params_opti = { # Parameters that controls optimizing, loss, scheduling.
+            'lr': 1e-4,
+            'weight_decay': 1e-4,
+            'step_size': 1,
+            'gamma': 1,
+        }
+        params_loss = { # Parameters that controls loss
+            'loss_func': 'standard', # Either 'standard' or 'custom'
+            'bonus_weight': 0,
+            'FN_w': 4, # False negative weight in custom loss
+            'FN_bound': 0.4, # False negative boundary in custom loss
+            'gaze_weight': 0, # 'gaze_weight' decides whether to use gaze info or not
+            #                         because final_loss = output_BCE_loss + gaze_weight * gaze_MSE_loss
+            #                         if gaze_weight is 0, the model will not use gaze info
+        }
+        params_prep = { # Parameters that controls preprocess
+            'thread_num': 4, # Note that different thread_num will lead to different result in dataset clipping
+            'util_percent': 0.1, # utilization percentage of the dataset
+            'tempDir': rootDir + 'temp/', # 'tempDir' is the directory to store clipped train dataset
+            'prep_every_epoch': True, # True or False
+            'transform': standard_transform,
+            'trainCateDictDir': rootDir + 'clip_info/' + train_DS_name + '_vid_category_dict.json',
+        }
+        params_test = { # Parameters that controls test process
+            'batch_size': 16,
+            'num_workers': 4,
+            'util_percent': 0.1, # utilization percentage of the dataset
+            'testClipsDir': rootDir + 'clipped_videos/' + test_DS_name + '_clip/',
+        }
+        params_train = { # Parameters that controls train process
+            'num_epochs': 3,
+            'batch_size': 16,
+            'num_workers': 4,
+            'seed': 64, # This seed if for the random shuffle of the dataset in training
+        }
 
-'''
-Serialized parameters start from here
-'''
-basic_param = { # Fundamental parameters
-    'rootDir': rootDir,
-    'train_DS_name': train_DS_name,
-    'test_DS_name': test_DS_name,
-    'checkpointDir': rootDir + 'checkpoints/',
-    'tryID': 'temp',
-    # 'tryID' specifies the folder name in checkpoint folder
-    #  - if tryID is None, the model will not save checkpoints
-    #  - if tryID is 'temp', the model will overwrite the temp folder
-}
-model_param = { # Parameters that controls model
-    'leaky': 0,
-    'model_path': None, # None stands for init a new model, otherwise load the model from the path
-    'arch': 'resnet18',
-}
-opti_param = { # Parameters that controls optimizing, loss, scheduling.
-    'lr': 1e-5,
-    'weight_decay': 1e-5,
-    'step_size': 1,
-    'gamma': 0.99,
-    'gaze_loss_weight': 1,
-    # 'gaze_loss_weight' decides whether to use gaze info or not
-    # because final_loss = output_BCE_loss + gaze_loss_weight * gaze_MSE_loss
-    #  - if gaze_loss_weight is 0, the model will not use gaze info
-}
-prep_param = { # Parameters that controls preprocess
-    'thread_num': 4,
-    'utilization_percentage': 0.1, # utilization percentage of the dataset
-    'tempDir': rootDir + 'temp/',
-    'prep_every_epoch': None, # None stands for let condition decide, True and False stand for enforcement
-    'transform': standard_transform,
-    'trainCateDictDir': rootDir + 'clip_info/' + train_DS_name + '_vid_category_dict.json',
-    'testClipsDir': rootDir + 'clipped_videos/' + test_DS_name + '_clip/',
-}
-test_param = { # Parameters that controls test process
-    'batch_size': 28,
-    'num_workers': 4,
-}
-train_param = { # Parameters that controls train process
-    'num_epochs': 10,
-    'batch_size': 6,
-    'num_workers': 4,
-}
+
+        self.basic = params_basic
+        self.model = params_model
+        self.opti = params_opti
+        self.prep = params_prep
+        self.test = params_test
+        self.train = params_train
+        self.loss = params_loss
--- Utils/Visualization.py
+++ Utils/Visualization.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import ipywidgets as widgets
+from tabulate import tabulate
 
 
 
@@ -30,14 +31,14 @@ class ProgressBar:
         display(self.layout)
     
 
-    def Update(self, info_text_value, increment=1):
+    def Update(self, info_text_value='', increment=1):
         self.progress += increment
         self.progress_bar.value = self.progress / self.total_len * 100
         self.info_text.value = info_text_value
     
 
 
-def PlotHistory(history, use_gaze, smooth_window_size=10):
+def PlotHistory(history, use_gaze, smooth_window_size=10, global_size=1):
     # Plot training history
     #  - if use_gaze is False, only 2 subplots needed to show out_loss and acc
     #  - if use_gaze is True, 4 subplots needed to show out_loss, acc, total_loss and gaze_loss
@@ -45,11 +46,18 @@ def PlotHistory(history, use_gaze, smooth_window_size=10):
     def smooth_data(data, window_size):
         if len(data) < window_size:
             return data
+        data = data[:]
+        # for _ in range(window_size - 1):
+        #     data.insert(0, data[0])
+        #     data.append(data[-1])
         return np.convolve(data, np.ones(window_size) / window_size, mode='valid')
 
     
+    figsize = (12 * global_size, 8 * global_size)
+    plt.rcParams['font.size'] = 12 * global_size
+
     # set up figure
-    fig = plt.figure(figsize=(12, 8))
+    fig = plt.figure(figsize=figsize)
     ax1 = fig.add_subplot(2, 2, 1)
     ax1.set_title('Out Loss')
     ax2 = fig.add_subplot(2, 2, 2)
@@ -63,7 +71,8 @@ def PlotHistory(history, use_gaze, smooth_window_size=10):
     # smooth data
     smoothed_history = {}
     for key in history.keys():
-        smoothed_history[key] = smooth_data(history[key], smooth_window_size)
+        if type(history[key]) == list:
+            smoothed_history[key] = smooth_data(history[key], smooth_window_size)
     
     # plot
     ax1.plot(smoothed_history['out_loss'], label='train')
@@ -77,3 +86,59 @@ def PlotHistory(history, use_gaze, smooth_window_size=10):
         ax4.legend()
     
     plt.show()
+
+
+
+def PlotResult(eval_history, train_history, global_size=1):
+
+    figsize = (12 * global_size, 8 * global_size)
+    plt.rcParams['font.size'] = 12 * global_size
+
+    len_per_epoch = train_history['len_per_epoch']
+    train_history['out_loss'] = [
+        np.mean(
+            train_history['out_loss'][i:i+len_per_epoch]
+        ) for i in range(0, len(train_history['out_loss']), len_per_epoch)
+    ]
+    train_history['acc'] = [
+        np.mean(
+            train_history['acc'][i:i+len_per_epoch]
+        ) for i in range(0, len(train_history['acc']), len_per_epoch)
+    ]
+
+    # set up figure
+    fig = plt.figure(figsize=figsize)
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax1.set_title('Out Loss')
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax2.set_title('Accuracy')
+    
+    # plot
+    ax1.plot(eval_history['out_loss'], label='eval')
+    ax1.plot(train_history['out_loss'], label='train')
+    ax1.legend()
+    ax2.plot(eval_history['acc'], label='eval')
+    ax2.plot(train_history['acc'], label='train')
+    ax2.legend()
+    
+    plt.show()
+
+
+
+def PrintBinConfusionMat(prediction, ground_truth):
+    # Use tabulate to print confusion matrix
+    #  - prediction and ground_truth are both 1D array of bool
+
+    # calculate confusion matrix
+    tp = np.sum(np.logical_and(prediction == True, ground_truth == True))
+    tn = np.sum(np.logical_and(prediction == False, ground_truth == False))
+    fp = np.sum(np.logical_and(prediction == True, ground_truth == False))
+    fn = np.sum(np.logical_and(prediction == False, ground_truth == True))
+    confusion_mat = [
+        ['',       'Actual T', 'Actual F'],
+        ['Pred T',  tp,         fp       ],
+        ['Pred F',  fn,         tn       ]
+    ]
+    print(' > Confusion Matrix:')
+    print(tabulate(confusion_mat, tablefmt='orgtbl'))
+    print(' > Total', len(prediction), 'samples')
